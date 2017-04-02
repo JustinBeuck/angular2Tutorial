f92a1bc7fed9d0471f0548ccc65998ee3575885c
Justin Beuck <jbeuck@yahoo.com>
2017-04-01 20:47:36 -0500

    added users list

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,15 +11,39 @@ import { Component } from '@angular/core'
 		</nav>
 	</header>
 
-		<div class="jumbotron">
-			<h1>Welcome to Our App!</h1>	
-		</div>
+	<main>
 
-		<footer>Copyright &copy; 2017</footer>
+	<div class="row">
+		<div class="col-sm-4">
+			<div *ngIf="users">
+				<ul class="list-group users-list" *ngFor="let user of users">
+					<li class="list-group-item">The user is {{user.name}} ({{ user.username }}).</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-sm-8">
+			<div class="jumbotron">
+				<h1>Welcome to Our App!</h1>
+					<p>{{ message }}<p>
+			</div>
+		</div>
+	</div>
+
+	</main>
+		<footer class="text-center">
+			Copyright &copy; 2017
+		</footer>
 	`,
 	styles: [
 	`.jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
 	`]
 })
 
-export class AppComponent {} 
+export class AppComponent {
+	message = "Hello!";
+	users = [
+		{ id: 25, name: 'justin', username: 'jbeuck' },
+		{	id: 26, name: 'Kelly', username: 'kbeuck' },
+		{	id: 27, name: 'Stephanie', username: 'sKidder' }
+	];
+} 
--- dist/app.component.js
+++ dist/app.component.js
@@ -9,13 +9,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
+        this.message = "Hello!";
+        this.users = [
+            { id: 25, name: 'justin', username: 'jbeuck' },
+            { id: 26, name: 'Kelly', username: 'kbeuck' },
+            { id: 27, name: 'Stephanie', username: 'sKidder' }
+        ];
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n\t<header>\n\t\t<nav class=\"navbar navbar-inverse\">\n\t\t\t<div class=\"navbar-header\">\n\t\t\t\t<a href=\"/\" class=\"navbar-brand\">My Angular 2 App!</a>\n\t\t\t</div>\n\t\t</nav>\n\t</header>\n\n\t\t<div class=\"jumbotron\">\n\t\t\t<h1>Welcome to Our App!</h1>\t\n\t\t</div>\n\n\t\t<footer>Copyright &copy; 2017</footer>\n\t",
+        template: "\n\t<header>\n\t\t<nav class=\"navbar navbar-inverse\">\n\t\t\t<div class=\"navbar-header\">\n\t\t\t\t<a href=\"/\" class=\"navbar-brand\">My Angular 2 App!</a>\n\t\t\t</div>\n\t\t</nav>\n\t</header>\n\n\t<main>\n\n\t<div class=\"row\">\n\t\t<div class=\"col-sm-4\">\n\t\t\t<div *ngIf=\"users\">\n\t\t\t\t<ul class=\"list-group users-list\" *ngFor=\"let user of users\">\n\t\t\t\t\t<li class=\"list-group-item\">The user is {{user.name}} ({{ user.username }}).</li>\n\t\t\t\t</ul>\n\t\t\t</div>\n\t\t</div>\n\t\t<div class=\"col-sm-8\">\n\t\t\t<div class=\"jumbotron\">\n\t\t\t\t<h1>Welcome to Our App!</h1>\n\t\t\t\t\t<p>{{ message }}<p>\n\t\t\t</div>\n\t\t</div>\n\t</div>\n\n\t</main>\n\t\t<footer class=\"text-center\">\n\t\t\tCopyright &copy; 2017\n\t\t</footer>\n\t",
         styles: [
             ".jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }\n\t"
         ]
