eed870cb6fea8cc269df1d255efc8707e858fb01
Justin Beuck <jbeuck@yahoo.com>
2017-04-02 12:20:51 -0500

    updated single component, cleaned up code

--- dist/app.component.js
+++ dist/app.component.js
@@ -25,10 +25,8 @@ var AppComponent = (function () {
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n\t<header>\n\t\t<nav class=\"navbar navbar-inverse\">\n\t\t\t<div class=\"navbar-header\">\n\t\t\t\t<a href=\"/\" class=\"navbar-brand\">My Angular 2 App!</a>\n\t\t\t</div>\n\t\t</nav>\n\t</header>\n\n\t<main>\n\n\t<div class=\"row\">\n\t\t<div class=\"col-sm-4\">\n\t\t\t<div *ngIf=\"users\">\n\t\t\t\t<ul class=\"list-group users-list\" >\n\t\t\t\t\t<li *ngFor=\"let user of users\" \n\t\t\t\t\t(click)=\"selectUser(user)\"\n\t\t\t\t\t[class.active]=\"user === activeUser\"\n\t\t\t\t\tclass=\"list-group-item\">\n\t\t\t\t\t\tThe user is {{user.name}} ({{ user.username }}).\n\t\t\t\t\t</li>\n\t\t\t\t</ul>\n\t\t\t</div>\n\t\t</div>\n\t\t<div class=\"col-sm-8\">\n\t\t\t<div class=\"jumbotron\" *ngIf=\"activeUser\">\n\t\t\t\t<h2>{{ activeUser.name }} <small>{{ activeUser.username }}</small></h2>\n\t\t\t</div>\n\n\t\t\t<div class=\"jumbotron\" *ngIf=\"!activeUser\">\n\t\t\t\t<span class=\"glyphicon glyphicon-hand-left\"></span>\n\t\t\t\t<h2>Choose a user</h2>\n\t\t\t</div>\n\t\t</div>\n\t</div>\n\n\t</main>\n\t\t<footer class=\"text-center\">\n\t\t\tCopyright &copy; 2017\n\t\t</footer>\n\t",
-        styles: [
-            "\n\t.users-list li { cursor: pointer; }\n\t.jumbotron .glyphicon { font-size: 80px;}\n\t"
-        ]
+        templateUrl: './app/app.component.html',
+        styleUrls: ['./app/app.component.css']
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
